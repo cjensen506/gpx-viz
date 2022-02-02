1145c2158aed06f9cd3065fc2d25d19b0a169017
cjensen506 <chris.jensen506@gmail.com>
2022-02-02 14:26:01 -0800

    animation over the course of a day w/ title change

--- main.R
+++ main.R
@@ -7,18 +7,10 @@ library(dplyr)
 library(gganimate)
 
 strava_export_name <- "export_584780"
-mt_hood_lat <- 45.37359
-mt_hood_lon <- -121.69581
+
 mt_hood_location <- c(45.37359, -121.69581)
 
-#gpx_data <- readGPX("./export_584780/activities/10186153.gpx")
-
-#gpx_track <- as.data.frame(gpx_data$tracks)
-#colnames(gpx_track) <- c("lon", "lat", "ele", "time")
-
-
-
-activities <- read_csv("./" + strava_export_name + "/activities.csv", col_types = cols(`Activity.Date` = col_datetime(format = "%b %d, %Y, %I:%M:%S %p")), name_repair = "universal")
+activities <- read_csv(paste("./", strava_export_name, "/activities.csv", sep = ""), col_types = cols(`Activity.Date` = col_datetime(format = "%b %d, %Y, %I:%M:%S %p")), name_repair = "universal")
 
 snow_sports <- activities[activities$`Activity.Type` %in% c("Nordic Ski", "Alpine Ski", "Backcountry Ski"),]
 
@@ -44,19 +36,43 @@ for(i in 1:nrow(snow_sports)){
       gpx_track$time <- as.POSIXct(gpx_track$time, format = "%Y-%m-%dT%H:%M:%S", tz = "UTC")
       start_time <- as.numeric(gpx_track[1,]$time)
       gpx_track$time_normal <- as.numeric(gpx_track$time) - start_time
+      
+      day_time <- strftime(gpx_track$time, format="%H:%M:%S")
+      ref_day <- "2022-01-01"
+      day_time <- as.POSIXct(paste(ref_day, day_time), tz = "PST")
+      ref_time <- as.POSIXct(paste(ref_day, "00:00:00"), tz = "PST")
+      gpx_track$time_24 <- as.numeric(day_time - ref_time)
+      if(max(gpx_track$time_24 > 24)){
+        gpx_track$time_24 <- gpx_track$time_24 / (60*60)
+        gpx_track[gpx_track$time_24 > 23,]$time_24 <- gpx_track[gpx_track$time_24 > 23,]$time_24 -24
+      }
+      
       snow_tracks <- rbind(snow_tracks, gpx_track)
     }
   }
 }
 
-#ggplot(snow_tracks, aes(x = lon, y = lat)) + coord_quickmap() + geom_point()
-ggplot(snow_tracks %>% filter(row_number() %% 10 == 1), aes(x = lon, y = lat, colour = factor(Activity.Type))) + coord_quickmap() + geom_point(size = .1)
 
 ggplot(snow_tracks %>% filter(row_number() %% 10 == 1), aes(x = lon, y = lat, colour = factor(Activity.Type))) + coord_quickmap() + geom_point(size = .1) + theme_void()
 
-ggplot(snow_tracks %>% filter(row_number() %% 10 == 1), aes(x = lon, y = lat, colour = factor(Activity.Type))) + coord_quickmap() + geom_point(size = .1) + theme_void()
+ski_hood_plot <- ggplot(snow_tracks %>% filter(row_number() %% 10 == 1), aes(x = lon, y = lat, colour = factor(Activity.Type), group = Activity.ID)) + coord_quickmap() + geom_path(size = .2, alpha = .25) + theme_void()
+ski_hood_plot <- ski_hood_plot + theme(legend.position = "none")
+ski_hood_plot
 
-ski_hood_plot <- ggplot(snow_tracks %>% filter(row_number() %% 10 == 1), aes(x = lon, y = lat, colour = factor(Activity.Type), group = Activity.ID)) + coord_quickmap() + geom_path(size = .2) + theme_void()
+ski_hood_animate <- ski_hood_plot + geom_point(size = 2) + transition_reveal(time_normal)
 
-ski_hood_plot + geom_point() + transition_reveal(time_normal)
+animate(ski_hood_animate, height = 800, width = 800)
+anim_save("Ski_Hood_samestart.gif")
+
+ski_hood_plot <- ggplot(snow_tracks %>% filter(row_number() %% 10 == 1), aes(x = lon, y = lat, colour = factor(Activity.Type), group = Activity.ID)) + coord_quickmap() + geom_path(size = .2, alpha = .25) + theme_void()
+ski_hood_plot <- ski_hood_plot + theme(legend.position = "none")
+ski_hood_plot
+
+#ski_hood_plot <- labs(title = 'Hour: {frame}')
+
+ski_hood_animate <- ski_hood_plot + geom_point(size = 2) + transition_reveal(time_24)
+ski_hood_animate <- ski_hood_animate + labs(title = 'Hour: {round(frame_along)}')
+
+animate(ski_hood_animate, height = 800, width = 800, duration = 24, fps = 15)
+anim_save("Ski_Hood_day.gif")
 
